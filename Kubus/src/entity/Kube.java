f26f7a170409bc24c81217de74abed0c56c0f976
Samuel <mayansymbol@gmail.com>
2016-04-15 13:14:06 -0400

    UHUHU

--- Kubus/src/entity/Kube.java
+++ Kubus/src/entity/Kube.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.HashMap;
+
 import graphics.Matrix4f;
 
 /*
@@ -15,16 +17,30 @@ import graphics.Matrix4f;
 
 public class Kube extends Entity
 {
+	private static final int TOP = 1;
+	private static final int FRONT = 2;
+	private static final int BOTTOM = 3;
+	private static final int BACK = 4;
+	private static final int LEFT = 5;
+	private static final int RIGHT = 6;
+	
 	private float tileLength;
 	//try 10x10 faces
 	private int faceLength;
-	//face that the player is on (top relative to camera)
-	private int topFace;
+	
+	private HashMap<Integer, Integer> faceMap;
 	
 	public Kube(int faceLength, float tileLength)
 	{
 		this.faceLength = faceLength;
 		this.tileLength = tileLength;
+		faceMap = new HashMap<Integer, Integer>();
+		faceMap.put(TOP, TOP);
+		faceMap.put(FRONT, FRONT);
+		faceMap.put(BOTTOM, BOTTOM);
+		faceMap.put(BACK, BACK);
+		faceMap.put(LEFT, LEFT);
+		faceMap.put(RIGHT, RIGHT);
 	}
 	
 	
@@ -40,34 +56,83 @@ public class Kube extends Entity
 	
 	private Matrix4f getRelativeRotation(int relativeFace)
 	{
-		
+		switch(relativeFace)
+		{
+		case TOP:
+			return new Matrix4f().initRotation(0, 1, 0);
+		case FRONT:
+			return new Matrix4f().initRotation(0, 0, 1);
+		case BOTTOM:
+			return new Matrix4f().initRotation(0, -1, 0);
+		case BACK:
+			return new Matrix4f().initRotation(0, 0, -1);
+		case LEFT:
+			return new Matrix4f().initRotation(-1, 0, 0);
+		case RIGHT:
+			return new Matrix4f().initRotation(1, 0, 0);
+		}
+		return null;
 	}
 	
-	private int getRelativeFace(int face)
+	
+	public void rotateTopForward()
 	{
-		if(topFace == 1)
+		for(int a = TOP; a < RIGHT; a++)
 		{
-			
+			int mappedValue = faceMap.get(a);
+			if(mappedValue != LEFT && mappedValue != RIGHT)
+			{
+				mappedValue++;
+				if(mappedValue > BACK)
+				{
+					mappedValue = TOP;
+				}
+			}
+			faceMap.put(a, mappedValue);
 		}
+	}
+	
+	public void rotateTopBackward()
+	{
+		for(int a = TOP; a < RIGHT; a++)
+		{
+			int mappedValue = faceMap.get(a);
+			if(mappedValue != LEFT && mappedValue != RIGHT)
+			{
+				mappedValue--;
+				if(mappedValue < FRONT)
+				{
+					mappedValue = BACK;
+				}
+			}
+			faceMap.put(a, mappedValue);
+		}
+	}
+	
+	public void rotateTopLeft()
+	{
+		for(int a = TOP; a < RIGHT; a++)
+		{
+			int mappedValue = faceMap.get(a);
+			if(mappedValue != BACK && mappedValue != FRONT)
+			{
+				mappedValue;
+				if(mappedValue < FRONT)
+				{
+					mappedValue = BACK;
+				}
+			}
+			faceMap.put(a, mappedValue);
+		}
+	}
+	
+	public void rotateTopRight()
+	{
+		
 	}
 	
 	public Matrix4f getFaceRotation(int face)
 	{
-		switch(topFace)
-		{
-		case 1:
-			return getRelativeRotation(getRelativeFace(face));
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		case 6:
-			break;
-		}
+		return getRelativeRotation(faceMap.get(face));
 	}
 }
