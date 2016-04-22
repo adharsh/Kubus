d6917dcc0d458be311c12b837a7365bad6333c67
Samuel <mayansymbol@gmail.com>
2016-04-22 13:09:16 -0400

    tile transformations

--- Kubus/src/entity/Tile.java
+++ Kubus/src/entity/Tile.java
@@ -79,30 +79,31 @@ public class Tile extends Entity
 		
 		
 		
-		float faceLength = cube.getFaceLength();
+		float faceLength = cube.getFaceLength() - 1;
+
+		float outValue = (float)cube.getFaceLength() / 2.f * cube.getTileLength();
 		faceLength /= 2.f;
-
-		float outValue = faceLength * cube.getTileLength() - (cube.getTileLength() / 2);
-
+		float xValue = ((float)xIndex - faceLength) * cube.getTileLength();
+		float yValue = ((float)yIndex - faceLength) * cube.getTileLength();
 		if(face == Kube.TOP)
 		{
-			renderTransform.setPosition(new Vector4f(((float)xIndex - faceLength) * cube.getTileLength(), outValue,
-					((float)yIndex - faceLength) * cube.getTileLength(), 1));
+			renderTransform.setPosition(new Vector4f(xValue, outValue,
+					yValue, 1));
 		}
 		else if(face == Kube.BOTTOM)
 		{
-			renderTransform.setPosition(new Vector4f(((float)xIndex - faceLength) * cube.getTileLength(), outValue,
-					((float)yIndex - faceLength) * cube.getTileLength(), 1));
+			renderTransform.setPosition(new Vector4f(xValue, outValue,
+					yValue, 1));
 		}
 		else if(face == Kube.FRONT)
 		{
-			renderTransform.setPosition(new Vector4f(((float)xIndex - faceLength) * cube.getTileLength(),
-					(((float)yIndex - faceLength)) * cube.getTileLength(), outValue, 1));
+			renderTransform.setPosition(new Vector4f(xValue,
+					-yValue, outValue, 1));
 		}
 		else if(face == Kube.RIGHT)
 		{
-			renderTransform.setPosition(new Vector4f(outValue, ((float)xIndex - faceLength) * cube.getTileLength(),
-					((float)yIndex - faceLength) * cube.getTileLength(), 1));
+			renderTransform.setPosition(new Vector4f(outValue, xValue,
+					yValue, 1));
 		}
 	}
 	
