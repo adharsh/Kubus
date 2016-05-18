3945cfbf5fb0646a689236ee1aaf74bf69a2a149
Samuel <mayansymbol@gmail.com>
2016-05-18 17:23:36 -0400

    n

--- Kubus/src/terrain/GoalTerrain.java
+++ Kubus/src/terrain/GoalTerrain.java
@@ -1,21 +1,36 @@
 package terrain;
 
+import java.io.IOException;
+
 import entity.Kube;
 import entity.Player;
 import entity.Tile;
+import graphics.Bitmap;
+import graphics.Matrix4f;
+import graphics.Mesh;
+import graphics.Renderer;
+import input.QMFLoader;
 
 public class GoalTerrain extends Terrain
 {
-//	private Kube map;
-//	
-//	static
-//	{
-//		
-//	}
+	private static final Mesh goalMesh;
+	private static Bitmap meshTexture;
+	
+	static
+	{
+		goalMesh = QMFLoader.loadQMF("res/QMF/goalflag.qmf");
+		try 
+		{
+			meshTexture = new Bitmap("res/flag.png");
+		} 
+		catch(IOException e) 
+		{
+			meshTexture = new Bitmap(1, 1);
+		}
+	}
 	
 	public GoalTerrain(Kube map, Tile tile) 
 	{
-//		this.map = map;
 		setPosition(tile.getPosition().add(tile.getHeightOffset()));
 		renderTransform.setRotation(map.getFaceRotation(tile.getFace()));
 		renderTransform.setScale(map.getTileLength(), map.getTileLength(), map.getTileLength());
@@ -33,4 +48,10 @@ public class GoalTerrain extends Terrain
 		player.setWon(true);
 	}
 
+	@Override
+	public void render(Renderer r, Matrix4f viewProjection)
+	{
+		super.render(r, viewProjection);
+		goalMesh.draw(r, viewProjection, renderTransform.getTransformation(), meshTexture);
+	}
 }
