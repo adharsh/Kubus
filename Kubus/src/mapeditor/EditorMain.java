c1ca360d4bd35cf01e3b316e91274bbf6ad6b9af
Samuel <mayansymbol@gmail.com>
2016-05-18 21:38:26 -0400

    ..

---
+++ Kubus/src/mapeditor/EditorMain.java
@@ -0,0 +1,9 @@
+package mapeditor;
+
+public class EditorMain
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ Kubus/src/mapeditor/Window.java
@@ -0,0 +1,59 @@
+package mapeditor;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.locks.Lock;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class BufferedPanel extends JPanel
+{
+	private BufferedImage frameBuffer;
+	private Graphics bufferGraphics;
+	public static Lock frameBufferLock;
+	
+	public BufferedPanel(int w, int h)
+	{
+		frameBuffer = new BufferedImage(w, h, BufferedImage.TYPE_4BYTE_ABGR);
+		bufferGraphics = frameBuffer.getGraphics();
+	}
+	
+	public Graphics getGraphics()
+	{
+		return bufferGraphics;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		frameBufferLock.lock();
+		g.drawImage(frameBuffer, 0, 0, null);
+		frameBufferLock.unlock();
+	}
+}
+
+public class Window
+{
+	private JFrame window;
+	private JPanel panel;
+	
+	
+	public Window(int w, int h)
+	{
+		window = new JFrame("Editor");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new BufferedPanel(w, h);
+		window.add(panel);
+		
+		window.pack();
+	}
+	
+	public void setVisible(boolean visible)
+	{
+		window.setVisible(visible);
+	}
+	
+	
+	
+}
